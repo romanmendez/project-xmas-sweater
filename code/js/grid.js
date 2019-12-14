b7e07b5df3cfd3374beafa587c009d623480a7a4
Román Méndez <mendezbeck@gmail.com>
2019-12-14 18:31:56 +0100

    added grid.js, working on grid

---
+++ code/js/grid.js
@@ -0,0 +1,17 @@
+let reindeer = new Figure(ctx, "reindeer");
+let picket1 = new Figure(ctx, "picket1");
+let picket2 = new Figure(ctx, "picket2");
+let picket3 = new Figure(ctx, "picket3");
+let darkStar = new Figure(ctx, "darkStar");
+let goat = new Figure(ctx, "goat");
+let anis = new Figure(ctx, "anis");
+let pitchfork = new Figure(ctx, "pitchfork");
+let dinosaur = new Figure(ctx, "dinosaur");
+let horns = new Figure(ctx, "horns");
+let tree = new Figure(ctx, "tree");
+let heart = new Figure(ctx, "heart");
+let ax = new Figure(ctx, "ax");
+let star = new Figure(ctx, "star");
+let triangle = new Figure(ctx, "triangle");
+
+let figuresArr = [reindeer, picket1, picket2, picket3, darkStar, goat, anis, pitchfork, dinosaur, horns, tree, heart, ax, star, triangle];
--- code/js/script.js
+++ code/js/script.js
@@ -1,7 +1,25 @@
 window.onload = () => {
-  let canvas = document.getElementById("canvas");
+  let canvas = document.getElementById("canvas0");
   let ctx = canvas.getContext("2d");
 
-  let reindeer = new Figure(ctx, "pitchfork", 20, 50);
-  reindeer.draw();
+  let reindeer = new Figure(ctx, "reindeer");
+  let picket1 = new Figure(ctx, "picket1");
+  let picket2 = new Figure(ctx, "picket2");
+  let picket3 = new Figure(ctx, "picket3");
+  let darkStar = new Figure(ctx, "darkStar");
+  let goat = new Figure(ctx, "goat");
+  let anis = new Figure(ctx, "anis");
+  let pitchfork = new Figure(ctx, "pitchfork");
+  let dinosaur = new Figure(ctx, "dinosaur");
+  let horns = new Figure(ctx, "horns");
+  let tree = new Figure(ctx, "tree");
+  let heart = new Figure(ctx, "heart");
+  let ax = new Figure(ctx, "ax");
+  let star = new Figure(ctx, "star");
+  let triangle = new Figure(ctx, "triangle");
+
+  let figuresArr = [reindeer, picket1, picket2, picket3, darkStar, goat, anis, pitchfork, dinosaur, horns, tree, heart, ax, star, triangle];
+  picket1.draw(0, 0);
+  picket1.draw(100, 0);
+  picket1.draw(200, 0);
 };
