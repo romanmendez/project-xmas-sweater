ba4fb11d2653a24c784b8b23f749b7939fc50a87
Román Méndez <mendezbeck@gmail.com>
2019-12-14 14:19:05 +0100

    created class in js to draw figures

--- code/js/script.js
+++ code/js/script.js
@@ -1,12 +1,7 @@
-let ctx = document.getElementById("canvas").getContext("2d");
+window.onload = () => {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
 
-function star() {
-  console.log("star");
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.quadraticCurveTo(25, 25, 25, 62.5);
-  ctx.stroke();
-  ctx.endPath();
-}
-
-star();
+  let reindeer = new Figure(ctx, "pitchfork", 20, 50);
+  reindeer.draw();
+};
