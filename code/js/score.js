60686629491b67b48a2d1aa05a5bb286988b5b97
Román Méndez <mendezbeck@gmail.com>
2019-12-16 19:24:42 +0100

    added score.js file

---
+++ code/js/score.js
@@ -0,0 +1,15 @@
+const ScoreBoard = {
+  ctx: undefined,
+  score: undefined,
+
+  init: function(ctx, score) {
+    this.ctx = ctx;
+    this.score = score;
+  },
+
+  draw: function(score) {
+    this.ctx.fillStyle = "black";
+    this.ctx.font = "40px sans-serif";
+    this.ctx.fillText(score, 50, 50);
+  }
+};
